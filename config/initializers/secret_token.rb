037cf171ba275b4f75a164c713dc0dae2813b025
Toni Haka-Risku <tojuhaka@gmail.com>
2013-09-24 22:29:21 +0300

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsTest::Application.config.secret_key_base = 'f935a11fec2b6ec315ee41eb267a83698c5ed06c052a1f69bc146cdce211d291958cab1dc62f33ebfacc04e7419df2609675858524c3918a2301d38643538f66'
---
+++ db/migrate/20130924185436_add_devise_columns_to_user.rb
@@ -0,0 +1,10 @@
+class AddDeviseColumnsToUser < ActiveRecord::Migration
+  def self.up
+    change_table :users do |t|
+      t.string :authentication_token
+    end
+  end
+  def self.down
+    t.remove :authentication_token
+  end
+end
